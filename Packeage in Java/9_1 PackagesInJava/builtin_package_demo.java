a1bc61171c5fa8aeb92e7372baf390b8dd31249a
Vivek Kumar <102277762+Bholuvivek@users.noreply.github.com>
2024-02-26 22:36:06 +0530

    commit

---
+++ Packeage in Java/9_1 PackagesInJava/builtin_package_demo.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class BuiltInPackageDemo{
+	public static void main(String [] args){
+		System.out.println("Please Enter Your Name:");
+		Scanner sc = new Scanner(System.in);
+		String my_name = sc.next();
+		System.out.println("Hello.. "+ my_name);
+		
+		
+	}
+}	
