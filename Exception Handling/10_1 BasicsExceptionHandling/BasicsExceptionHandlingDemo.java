9d88c275ef67b3d028fdd57ad63401e1eb7010f9
Vivek Kumar <102277762+Bholuvivek@users.noreply.github.com>
2024-02-28 21:44:12 +0530

    commit

---
+++ Exception Handling/10_1 BasicsExceptionHandling/BasicsExceptionHandlingDemo.java
@@ -0,0 +1,33 @@
+class Demo{
+	int a[] = new int[5];
+	
+	public void set(){
+		for (int i = 0;i<5;i++)
+			a[i] = i+2;
+	}
+	
+	public void print(){
+		for (int i = 0;i<=15;i++)
+			System.out.println(a[i]);
+	}
+	
+	public static void main(String [] args){
+		
+		//obj.a[0] = 20;
+		//System.out.println("Setting of a[0] done");
+		//obj.a[25] = 20;
+		//System.out.println("Setting of a[25] done");
+		//try{
+		//obj.print();
+		int b = 10;
+		c = sc.nextInt();
+		int a = b/c;
+		//}
+		//catch(Exception e){
+	//	}
+		System.out.println("After Calling Print Method");
+		int x = 10;
+		System.out.println("X =" + x);
+
+	}
+}
